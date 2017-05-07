d090e3f4d0af0f69955f27999d221612e49be938
Megumi Page <megumi.page@gmail.com>
2017-05-07 14:05:08 -0700

    * WIP with modifying list of dicts to dict of dicts

--- dlToDd.py
+++ dlToDd.py
@@ -19,29 +19,30 @@ def main():
     endingPartialNumber = int(args.lastPartialNumber)
     ordinal = "{" + ":0{}d".format(len(args.lastPartialNumber)) + "}"
 
-    currentNum = 1 
+    currentNum = 1
     while currentNum < endingPartialNumber:
-        trumpJson = generateOldIntermediateFileNames(baseName + ordinal.format(currentNum))[0]
-        if os.path.exists(trumpJson):
+        fileDict = generateOldIntermediateFileNames(baseName + ordinal.format(currentNum))
+        for i in range(1,3):
+            if os.path.exists(historyJson):
 
-            with codecs.open(trumpJson, "r", "utf-8") as file:
-                fileList = json.load(file)
+                with codecs.open(historyJson, "r", "utf-8") as file:
+                    fileList = json.load(file)
 
-            # do need to look at all users at once? only when using to vote for tweets, so search through each file then
-            #       deduping was done jankily, make sure deduping is included in flow of scripts -> rawSplit.py
-            # do need to look at all user histories at once? search through each file by user id when necesary
-            #   reformat histories to be dict of userId => [texts]
-            #            users to be     dict of userId => [screen_name, alignment, class]
-            #            trumpTweets     dict of userId => { tweetId => text, tweetId => text }
-            # do need to look at all trump tweets at once? loop through files when voting via hashtags, when voting for users, when auxiliary voting via 
-            masterFileDict = dict()
-            for tweet in fileList:
-                if tweet["id"] not in masterFileDict:
-                    tweet.pop("id")
-                    masterFileDict[tweet["id"]] = tweet # ensure no duplicate tweets
-        else:
-            print "Error:", trumpJson, "does not exist in current directory"
-            exit()
+                # do need to look at all users at once? only when using to vote for tweets, so search through each file then
+                #       deduping was done jankily, make sure deduping is included in flow of scripts -> rawSplit.py
+                # do need to look at all user histories at once? search through each file by user id when necesary
+                #   reformat histories to be dict of userId => [texts]
+                #            users to be     dict of userId => [screen_name, alignment, class]
+                #            trumpTweets     dict of userId => { tweetId => text, tweetId => text }
+                # do need to look at all trump tweets at once? loop through files when voting via hashtags, when voting for users, when auxiliary voting via 
+                masterFileDict = dict()
+                for tweet in fileList:
+                    if tweet["id"] not in masterFileDict:
+                        tweet.pop("id")
+                        masterFileDict[tweet["id"]] = tweet # ensure no duplicate tweets
+            else:
+                print "Error:", topicJson, "does not exist in current directory"
+                exit()
 
 
             # generator???? sequential read-in of hashtagFile
